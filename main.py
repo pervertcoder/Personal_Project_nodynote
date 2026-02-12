b7fbef69d4c00ae57463c758a53005ebad6e65f9
pervertcoder <lance60702@gmail.com>
2026-02-12 15:51:05 +0800

    research websocket + create welcome page frontend

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from fastapi import *
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+# from fastapi import websocket
+from typing import List
 
 app = FastAPI()
 
-app.mount("/statics", StaticFiles(directory="statics"))
+connections : List[WebSocket] = []
 
+@app.websocket("/ws/note/{note_id}")
+async def websocket_endpoint(websocket:WebSocket, note_id:int):
+    await websocket.accept()
+    connections.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            for conn in connections:
+                if conn != websocket:
+                    await conn.send_text(data)
+    except:
+        connections.remove(websocket)
+
+app.mount("/statics", StaticFiles(directory="statics"))
 # Static Pages
 @app.get("/")
 async def index(request: Request):
